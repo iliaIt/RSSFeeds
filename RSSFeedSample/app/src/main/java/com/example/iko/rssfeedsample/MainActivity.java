f17d9699c804a2beca3841858360f77f06fd740a
ilia <ilia_it@abv.bg>
2016-04-15 14:24:58 +0300

    Created simple list for rssFeedURLs ...

--- RSSFeedSample/app/src/main/java/com/example/iko/rssfeedsample/MainActivity.java
+++ RSSFeedSample/app/src/main/java/com/example/iko/rssfeedsample/MainActivity.java
@@ -6,17 +6,23 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.iko.rssfeedsample.CustomThreads.RSSFeedThread;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemClickListener {
 
     final public String FEED_1 = "http://www.plovdiv24.bg/rss.php";
     private Button rssFeed1;
     private TextView info;
     private TextView rssFeedXML;
+    private ListView rssFeedsList;
+    String list[] = {"Name1", "name2", "name6", "name6", "name5", "name4", "name3"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +33,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         rssFeed1.setOnClickListener(this);
         info = (TextView) findViewById(R.id.textView_Info);
         rssFeedXML = (TextView) findViewById(R.id.textView_RssFeedXML);
+        rssFeedsList = (ListView) findViewById( R.id.listView_FeedsUrls );
 
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>( this, android.R.layout.simple_list_item_1, list );
+        rssFeedsList.setAdapter( adapter );
+        rssFeedsList.setOnItemClickListener(this);
     }
 
     @Override
@@ -81,5 +91,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main_menu, menu);
         return true;
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        TextView tview = (TextView) view;
+        Toast.makeText(MainActivity.this, tview.getText() , Toast.LENGTH_SHORT).show();
     }
 }
